2cbeed3b2af5c71ecd6cd40f3d46849f299db7f3
Mobola847 <139622920+Mobola847@users.noreply.github.com>
2023-10-18 04:24:31 +0100

    Add files via upload

---
+++ dframe.R
@@ -0,0 +1,15 @@
+## Assignment
+
+myNewData <- data.frame(person = c("Stan", "Francine", "Steve", "Roger", "Hayley","Klaus"),
+                        sex = factor(c("M", "F", "M", "M", "F", "M")),
+                        funny = factor(c("High", "Med", "Low", "High", "Med", "Med"))
+)
+
+
+newAge <- c(41, 41, 15, 1600, 21, 60)
+
+myNewData <- cbind(myNewData, newAge)
+
+
+
+
